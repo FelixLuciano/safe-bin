bbfc23055a51820e42db746abaaad0017ab7e844
FelixLuciano <felix.lucianodias@gmail.com>
2023-05-19 19:42:58 -0300

    feat: Add image zooming with medium-zoom

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,6 +1,7 @@
 // https://vitepress.dev/guide/custom-theme
 import DefaultTheme, { VPHomeHero } from 'vitepress/theme'
 import layout from './Layout.vue'
+import ImgZoom from './components/ImgZoom.vue'
 import './style.css'
 
 export default {
@@ -10,5 +11,6 @@ export default {
 
   enhanceApp({ app, router, siteData }) {
     app.component('VPDocHero', VPHomeHero)
+    app.component('ImgZoom', ImgZoom)
   }
 }
