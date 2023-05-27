496d27efa3dadf1877e8d017cb064a7a8bef78cf
FelixLuciano <felix.lucianodias@gmail.com>
2023-05-26 23:52:36 -0300

    refactor: add provider version and code format

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,7 +1,7 @@
 resource "aws_dynamodb_table" "data" {
-  name           = "safebin_data"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "key_id"
+  name         = "safebin_data"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "key_id"
 
   attribute {
     name = "key_id"
